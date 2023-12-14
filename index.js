8cc423e50eff58a01bbd09ed12385df7f5b954e2
Studio Elsewhere <studioelsewhere@studios-mini.mynetworksettings.com>
2023-12-13 21:54:24 -0500

    wasi-backend

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+// Endpoint to get all organizations
+app.get('/organizations', async (req, res) => {
+    try {
+        const organizations = await prisma.organizations.findMany();
+        res.json(organizations);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
